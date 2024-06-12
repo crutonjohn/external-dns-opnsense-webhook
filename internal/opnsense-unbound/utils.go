720fe637d1551ee8b6c40540a71ecfba7af8dc0f
Thomas Way <thomas@6f.io>
2024-06-12 12:32:03 +0100

    chore: safely construct URLs
    There was no validation on the provided URL before, and they were
    constructed with string formatting which is prone to issues.

--- internal/opnsense-unbound/utils.go
+++ internal/opnsense-unbound/utils.go
@@ -2,17 +2,6 @@ package opnsense
 
 import "strings"
 
-// FormatUrl formats a URL with the given parameters.
-func FormatUrl(path string, params ...string) string {
-	segments := strings.Split(path, "%s")
-	for i, param := range params {
-		if param != "" {
-			segments[i] += param
-		}
-	}
-	return strings.Join(segments, "")
-}
-
 // UnboundFQDNSplitter splits a DNSName into two parts,
 // [0] Being the top level hostname
 // [1] Being the subdomain/domain
