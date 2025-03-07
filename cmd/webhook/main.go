fc89abc07bb7694a9550a96c9f102263aeef0036
Curtis John <admin@curtisrayjohn.com>
2025-03-07 17:03:50 -0500

    fix(pprof): add library duh

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/crutonjohn/external-dns-opnsense-webhook/cmd/webhook/init/configuration"
 	"github.com/crutonjohn/external-dns-opnsense-webhook/cmd/webhook/init/dnsprovider"
@@ -30,8 +31,8 @@ func main() {
 
 	// Start pprof server in a separate goroutine
 	go func() {
-		log.Println("Starting pprof server on :6060")
-		if err := http.ListenAndServe(":6060", nil); err != nil {
+		log.Println("Starting pprof server on 0.0.0.0:6060")
+		if err := http.ListenAndServe("0.0.0.0:6060", nil); err != nil {
 			log.Fatalf("pprof server failed: %v", err)
 		}
 	}()
