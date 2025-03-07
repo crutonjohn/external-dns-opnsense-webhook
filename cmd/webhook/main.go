f1b3fd594a97518403d3f71a35eab464610bf464
Curtis John <admin@curtisrayjohn.com>
2025-03-07 16:24:08 -0500

    feat(pprof): initial commit of pprof

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/crutonjohn/external-dns-opnsense-webhook/cmd/webhook/init/configuration"
 	"github.com/crutonjohn/external-dns-opnsense-webhook/cmd/webhook/init/dnsprovider"
@@ -26,6 +27,14 @@ func main() {
 	fmt.Printf(banner, Version, Gitsha)
 
 	logging.Init()
+
+	// Start pprof server in a separate goroutine
+	go func() {
+		log.Println("Starting pprof server on :6060")
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatalf("pprof server failed: %v", err)
+		}
+	}()
 
 	config := configuration.Init()
 	provider, err := dnsprovider.Init(config)
